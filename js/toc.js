7b07dc6557ea540ef82ec1c3a8a70c7608812c86
ardiesan <113565882+ardiesan@users.noreply.github.com>
2024-08-18 00:20:49 +0800

    removed let

--- js/toc.js
+++ js/toc.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 var coll = document.getElementsByClassName("collapsible");
-for (let i = 0; i <= coll.length; i++) {
+for (var i = 0; i <= coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
